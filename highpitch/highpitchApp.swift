30d99e31e38d1ce4516c16f430998ec252e03e8d
yuncoffee <dong072815@gmail.com>
2023-09-11 20:58:07 +0900

    Initial Commit

---
+++ highpitch/highpitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  highpitchApp.swift
+//  highpitch
+//
+//  Created by Yun Dongbeom on 2023/09/11.
+//
+
+import SwiftUI
+
+@main
+struct highpitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
