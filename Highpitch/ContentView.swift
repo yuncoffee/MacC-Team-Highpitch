ac64ba27f0384fbd9688f400b662931a8aead85e
yuncoffee <dong072815@gmail.com>
2023-09-11 23:19:58 +0900

    [Chore] add ContentView

---
+++ Highpitch/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Highpitch
+//
+//  Created by Yun Dongbeom on 2023/09/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
--- Highpitch/HighpitchApp.swift
+++ Highpitch/HighpitchApp.swift
@@ -11,6 +11,7 @@ import SwiftUI
 struct HighpitchApp: App {
     var body: some Scene {
         WindowGroup {
+            ContentView()
         }
     }
 }
