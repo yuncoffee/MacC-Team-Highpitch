f46d8717ae08e0dfceb052314c290299357978bc
yuncoffee <dong072815@gmail.com>
2023-09-11 22:47:20 +0900

    [Test] local branch test #5

--- highpitch/ContentView.swift
+++ highpitch/ContentView.swift
@@ -21,6 +21,7 @@ struct ContentView: View {
                 .foregroundColor(.accentColor)
             Text("Hello, iOS")
             #endif
+            Text("Branch Test")
         }
         .padding()
     }
