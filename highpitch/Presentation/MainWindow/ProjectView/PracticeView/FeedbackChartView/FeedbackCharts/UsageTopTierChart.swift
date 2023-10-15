1ca3974b9798c04b6d9d6b9e7338d14d948c8e65
leeyongjun604 <daniel_604@naver.com>
2023-10-15 13:05:40 +0900

    [Feat] #27 - each filler word count

--- highpitch/Presentation/MainWindow/ProjectView/PracticeView/FeedbackChartView/FeedbackCharts/UsageTopTierChart.swift
+++ highpitch/Presentation/MainWindow/ProjectView/PracticeView/FeedbackChartView/FeedbackCharts/UsageTopTierChart.swift
@@ -27,6 +27,26 @@ struct UsageTopTierChart: View {
 
 extension UsageTopTierChart {
     
+    // MARK: 습관어 사용 횟수를 리턴합니다.
+    func getFillerRate() -> Int {
+        let fillerWordList = FillerWordList()
+        // index에 맞게 fillerword 사용 횟수를 확인합니다.
+        var fillerCount = [Int](repeating: 0, count: 22)
+        var messagesArray: [[String]] = []
+        for utterence in data.utterences {
+            messagesArray.append(utterence.message.components(separatedBy: " "))
+        }
+        for messageArray in messagesArray {
+            for message in messageArray {
+                for index in 0..<fillerWordList.defaultList.count {
+                    if fillerWordList.defaultList[index] == message {
+                        fillerCount[index] += 1
+                    }
+                }
+            }
+        }
+        return 0
+    }
 }
 
 #Preview {
