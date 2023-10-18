1b6059619b28961b767ce3c3b8106eb640281c00
musung <musungkim1352@gmail.com>
2023-10-18 17:00:26 +0900

    [Fix]#49 오디오 매니저 변경 및 기능 추가

--- highpitch/Managers/AudioManager.swift
+++ highpitch/Managers/AudioManager.swift
@@ -6,28 +6,31 @@
 //
 
 import AVFoundation
+enum AudioError: Error {
+    case audioNotFoundErr
+}
 
-class AudioManager: NSObject, AVAudioPlayerDelegate {
-    /// 음성메모 녹음 관련 프로퍼티
-    var audioRecorder: AVAudioRecorder?
-    
-    /// 음성메모 재생 관련 프로퍼티
-    var audioPlayer: AVAudioPlayer?
-    
+class AudioRecorderManager: NSObject, AVAudioPlayerDelegate {
+/// 음성메모 녹음 관련 프로퍼티
+var audioRecorder: AVAudioRecorder?
+
+/// 음성메모 재생 관련 프로퍼티
+var audioPlayer: AVAudioPlayer?
+
 }
 
 // MARK: - 음성메모 녹음 관련 메서드
-extension AudioManager {
+extension AudioRecorderManager {
     func startRecording() {
-        // MARK: 파일 이름 전략은 추후에 확정
+        //MARK: 파일 이름 전략은 추후에 확정
         let fileURL = getPath(fileName: "test")
         let settings = [
-            AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
-            AVSampleRateKey: 12000,
-            AVNumberOfChannelsKey: 1,
-            AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+        AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+        AVSampleRateKey: 12000,
+        AVNumberOfChannelsKey: 1,
+        AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
         ]
-        
+
         do {
             audioRecorder = try AVAudioRecorder(url: fileURL, settings: settings)
             audioRecorder?.record()
@@ -35,11 +38,11 @@ extension AudioManager {
             print("녹음 중 오류 발생: \(error.localizedDescription)")
         }
     }
-    
+
     func stopRecording() {
         audioRecorder?.stop()
     }
-    
+
     private func getDocumentsDirectory() -> URL {
         let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
         return paths[0]
@@ -47,36 +50,42 @@ extension AudioManager {
 }
 
 // MARK: - 음성메모 재생 관련 메서드
-extension AudioManager {
-    func startPlaying(url: URL) {
+extension AudioRecorderManager {
+    func registerAudio(url: URL) throws {
         do {
             audioPlayer = try AVAudioPlayer(contentsOf: url)
             audioPlayer?.delegate = self
-            audioPlayer?.play()
         } catch {
             print("재생 중 오류 발생: \(error.localizedDescription)")
         }
     }
-    func playAt(atTime: Double) {
-        let offset = atTime/100
-        audioPlayer?.currentTime = offset
+    func play() {
         audioPlayer?.play()
     }
+    func playAt(atTime: Double) {
+        let offset = atTime/1000
+        audioPlayer?.currentTime = offset
+    }
+    ///
+    func playAfter(second: Double) {
+        audioPlayer?.currentTime = second + (audioPlayer?.currentTime ?? 0)
+    }
+    ///
     func stopPlaying() {
         audioPlayer?.stop()
     }
-    
+
     func pausePlaying() {
         audioPlayer?.pause()
     }
-    
+
     func resumePlaying() {
         audioPlayer?.play()
     }
-    
-    func getPath(fileName:String) -> URL {
+    func getPath(fileName: String) -> URL {
         let dataPath = getDocumentsDirectory()
-            .appendingPathComponent("HighPitch").appendingPathComponent("Audio")
+            .appendingPathComponent("HighPitch")
+            .appendingPathComponent("Audio")
         do {
             try FileManager.default
                 .createDirectory(at: dataPath, withIntermediateDirectories: true, attributes: nil)
@@ -85,4 +94,8 @@ extension AudioManager {
         }
         return dataPath.appendingPathComponent(fileName + ".m4a")
     }
+    func getDuration() -> Double {
+        return audioPlayer?.duration ?? 0
+    }
+
 }
