6ec62c803836ff0778c910465857a38b507d2289
ljh <dusk5588@naver.com>
2023-10-22 18:16:39 +0900

    Practice View Design End

--- highpitch/Presentation/MainWindow/ProjectView/PracticesTabItem.swift
+++ highpitch/Presentation/MainWindow/ProjectView/PracticesTabItem.swift
@@ -16,27 +16,29 @@ struct PracticesTabItem: View {
     
     var body: some View {
         @Bindable var projectManager = projectManager
+        HStack {
+            Text("0개 선택")
+                .systemFont(.footnote, weight: .semibold)
+                .foregroundStyle(Color.primary500)
+            Spacer()
+            Button("편집하기") {
+                
+            }
+            .systemFont(.footnote, weight: .semibold)
+            .foregroundStyle(Color("8B6DFF"))
+            .buttonStyle(.plain)
+        }
+        
         NavigationStack(path: $projectManager.path) {
             
-            HStack {
-                Text("0개 선택")
-                    .systemFont(.footnote, weight: .semibold)
-                    .foregroundStyle(Color.primary500)
-                Spacer()
-                Button("편집하기") {
-                    
-                }
-                .systemFont(.footnote, weight: .semibold)
-                .foregroundStyle(Color("8B6DFF"))
-                .buttonStyle(.plain)
-            }
             
             if let project = projectManager.current {
                 List {
                     ForEach(Array(project.practices.sorted().enumerated()).reversed(), id: \.element.id) { index, practice in
                         
-                        
                         HStack(alignment: .center, spacing: .HPSpacing.xxsmall) {
+                            
+                            // 몇번째 연습
                             VStack(alignment:.leading, spacing: 0) {
                                 // let reversedIndex = project.practices.count - index
                                 Text(Date().createAtToPracticeDate(input: practice.creatAt))
@@ -45,6 +47,7 @@ struct PracticesTabItem: View {
                                 NavigationLink("\(PracticesTabItem().indexToOrdinalNumber(index: index))번째 연습", value: practice)
                                     .systemFont(.title, weight: .semibold)
                                     .foregroundStyle(Color.HPTextStyle.darker)
+                                    .border(Color.red, width:2)
                                 HPStyledLabel(content: "1시간 30분 48초 소요")
                             }
                             .padding(.horizontal, .HPSpacing.medium)
@@ -57,6 +60,7 @@ struct PracticesTabItem: View {
                                 .frame(width: 1, height: 100) // 너비와 높이를 조절
                                 .padding(.vertical, 5) // 원하는 간격으로 조절
                             
+                            // 평균레벨 습관어 발화속도
                             VStack(alignment: .trailing) {
                                 HStack {
                                     Text("자세히 보기")
@@ -83,9 +87,11 @@ struct PracticesTabItem: View {
                         .listRowSeparator(.hidden)
                         .background(Color.white)
                         .clipShape(RoundedRectangle(cornerRadius: 12, style: .continuous))
-                        .border(Color.red, width: 2)
-                        
+                        .listRowBackground(Color.HPComponent.mainWindowDetailsBackground)
                     }
+                }
+                .toolbar{
+                    
                 }
                 // MARK: 연습 삭제하기 버튼 임시로 만들었습니다. 순서 상관없이 인덱스 0번째꺼 지우는 코드니깐 나중에 수정 필요!!
                 .contextMenu {
@@ -93,22 +99,17 @@ struct PracticesTabItem: View {
                         projectManager.current?.practices.remove(at: 0)
                     }
                 }
-                .listRowBackground(Color.HPComponent.mainWindowDetailsBackground)
                 .listStyle(.plain)
                 .navigationDestination(for: PracticeModel.self) { practice in
                     PracticeView(practice: practice)
                         .environment(MediaManager())
                 }
                 .navigationTitle("Practice")
-        }
-        else {
-            VStack {
-                Text("아직 연습한 기록이 없어요")
+                .scrollContentBackground(.hidden)
             }
         }
     }
-}
-
+    
 }
 
 // #Preview {
