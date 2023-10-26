bb398dd31d15fae4e1c5e58e7bfc0c30df995599
ljh <dusk5588@naver.com>
2023-10-26 17:38:08 +0900

    [Refactor] DataModels 밑에 SampleProjectJSON 클래스 삭제

--- highpitch/Models/DataModels/Practice.swift
+++ highpitch/Models/DataModels/Practice.swift
@@ -7,14 +7,14 @@
 
 import Foundation
 
-struct Practice: Codable {
-    var id = UUID()
-    var audioPath: URL
-    var utterances: [Utterance]
-}
-
-extension Practice: Hashable {
-    static func == (lhs: Practice, rhs: Practice) -> Bool {
-        lhs.id == rhs.id
-    }
-}
+//struct Practice: Codable {
+//    var id = UUID()
+//    var audioPath: URL
+//    var utterances: [Utterance]
+//}
+//
+//extension Practice: Hashable {
+//    static func == (lhs: Practice, rhs: Practice) -> Bool {
+//        lhs.id == rhs.id
+//    }
+//}
--- highpitch/Models/DataModels/Project.swift
+++ highpitch/Models/DataModels/Project.swift
@@ -7,24 +7,24 @@
 
 import Foundation
 
-struct Project {
-    let id = UUID()
-    var projectName: String
-    var creatAt: String
-    var keynotePath: URL?
-    var practices: [Practice]
-    var keynoteCreation: String
-}
-
-extension Project: Equatable {}
-extension Project: Hashable {}
-extension Project {
-    init() {
-        self.init(
-            projectName: "새 프로젝트",
-            creatAt: "--임시--",
-            practices: [],
-            keynoteCreation: "--임시--"
-        )
-    }
-}
+//struct Project {
+//    let id = UUID()
+//    var projectName: String
+//    var creatAt: String
+//    var keynotePath: URL?
+//    var practices: [Practice]
+//    var keynoteCreation: String
+//}
+//
+//extension Project: Equatable {}
+//extension Project: Hashable {}
+//extension Project {
+//    init() {
+//        self.init(
+//            projectName: "새 프로젝트",
+//            creatAt: "--임시--",
+//            practices: [],
+//            keynoteCreation: "--임시--"
+//        )
+//    }
+//}
