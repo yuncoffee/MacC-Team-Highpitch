0f774b948c508665a5c56f8aa97d025e64535e9f
yuncoffee <dong072815@gmail.com>
2023-10-29 00:06:42 +0900

    [Refactor] MenubarExtraHeader의 메서드 -> ProjectManager로 이동 위치 이야기 할 필요 있음 #95

--- highpitch/Presentation/MenubarExtra/MenubarExtraHeader/MenubarExtraHeader.swift
+++ highpitch/Presentation/MenubarExtra/MenubarExtraHeader/MenubarExtraHeader.swift
@@ -66,7 +66,7 @@ struct MenubarExtraHeader: View {
 // MARK: Views
 extension MenubarExtraHeader {
     @ViewBuilder
-    var leftButtonGroup: some View {
+    private var leftButtonGroup: some View {
         HStack(spacing: .HPSpacing.xxsmall) {
             openMainWindowButton
             openSettingWindowButton
@@ -74,7 +74,7 @@ extension MenubarExtraHeader {
     }
     
     @ViewBuilder
-    var openMainWindowButton: some View {
+    private var openMainWindowButton: some View {
         HPButton(type: .text, size: .medium, color: .HPGray.system800) {
             openSelectedProject()
         } label: { type, size, color, expandable in
@@ -91,7 +91,7 @@ extension MenubarExtraHeader {
     }
     
     @ViewBuilder
-    var openSettingWindowButton: some View {
+    private var openSettingWindowButton: some View {
         HPButton(type: .text, size: .medium, color: .HPGray.system800) {
             try? openSettings()
         } label: { type, size, color, expandable in
@@ -108,7 +108,7 @@ extension MenubarExtraHeader {
     }
     
     @ViewBuilder
-    var rightButtonGroup: some View {
+    private var rightButtonGroup: some View {
         HStack(spacing: .HPSpacing.xsmall + .HPSpacing.xxxxsmall) {
             playAndPauseButton
             stopButton
@@ -116,7 +116,7 @@ extension MenubarExtraHeader {
     }
     
     @ViewBuilder
-    var playAndPauseButton: some View {
+    private var playAndPauseButton: some View {
         let labels = if !mediaManager.isRecording {
             (
                 label:"연습 시작",
@@ -153,7 +153,7 @@ extension MenubarExtraHeader {
     }
     
     @ViewBuilder
-    var stopButton: some View {
+    private var stopButton: some View {
         let content = (label: "끝내기", icon: "stop.fill")
         HPButton(type: .text, size: .medium, color: .HPSecondary.base) {
             Task {
@@ -184,140 +184,29 @@ extension MenubarExtraHeader {
     // MARK: - 연습 시작.
     private func playPractice() {
         print("------연습이 시작되었습니다.-------")
-        /// 선택된 키노트가 있을 때
-        if let selectedKeynote = selectedKeynote {
-            Task {
-                await appleScriptManager.runScript(.startPresentation(fileName: selectedKeynote.path))
-            }
-        } else {
-            /// 선택된 키노트가 없을 때
-        }
-        projectManager.temp = selectedProject?.persistentModelID
-        keynoteManager.temp = selectedKeynote
-        mediaManager.fileName = Date().makeM4aFileName()
-        mediaManager.startRecording()
+        projectManager.playPractice(
+            selectedKeynote: selectedKeynote,
+            selectedProject: selectedProject,
+            appleScriptManager: appleScriptManager,
+            keynoteManager: keynoteManager,
+            mediaManager: mediaManager
+        )
     }
     
     // MARK: - 연습 일시중지
     private func pausePractice() {
-        
+        projectManager.pausePractice()
     }
     
     // MARK: - 연습 끝내기
     @MainActor
     private func stopPractice() {
-        if !mediaManager.isRecording {
-            return
-        }
-        print("녹음 종료")
-        mediaManager.stopRecording()
-        /// mediaManager.fileName에 음성 파일이 저장되어있을거다!!
-        /// 녹음본 파일 위치 : /Users/{사용자이름}/Documents/HighPitch/Audio.YYYYMMDDHHMMSS.m4a
-        /// ReturnZero API를 이용해서 UtteranceModel완성
-        Task {
-            let newUtteranceModels = await makeNewUtterances()
-            /// 아무말도 하지 않았을 경우 종료한다.
-            if newUtteranceModels.isEmpty {
-                print("none of words!")
-                return
-            }
-            /// 시작할 때 프로젝트 세팅이 안되어 있을 경우, 새 프로젝트를 생성 하고, temp에 반영한다.
-            /// temp는 새로 만들어진 ProjectModel.persistentModelID 을 들고 있다.
-            if projectManager.temp == nil {
-                makeNewProject()
-            }
-            /// 생성한 ID로 프로젝트 모델을 가져온다.
-            guard let id = projectManager.temp else { return }
-            guard let tempProject = modelContext.model(for: id) as? ProjectModel else { return }
-            let newPracticeModel = makeNewPractice(project: tempProject, utterances: newUtteranceModels)
-            /// 프로젝트에 추가한다.
-            tempProject.practices.append(newPracticeModel)
-            
-            /// @@@@@@@@ summary를 생성한다. 변경 필요! @@@@@@@@
-            practiceManager.current = newPracticeModel
-            practiceManager.getPracticeDetail(practice: newPracticeModel)
-            /// @@@@@@@@ summary를 생성한다. 변경 필요! @@@@@@@@
-            
-            projectManager.temp = nil
-            
-            if projectManager.current == nil {
-                projectManager.current = tempProject
-            }
-            NotificationManager.shared.sendNotification(
-                name: practiceManager.current?.practiceName ?? "err"
-            )
-        }
-    }
-    
-    private func makeNewUtterances() async -> [UtteranceModel] {
-        var result: [UtteranceModel] = []
-        do {
-            let tempUtterances: [Utterance] = try await ReturnzeroAPI()
-                .getResult(filePath: mediaManager.getPath(fileName: mediaManager.fileName).path())
-            for tempUtterance in tempUtterances {
-                result.append(
-                    UtteranceModel(
-                        startAt: tempUtterance.startAt,
-                        duration: tempUtterance.duration,
-                        message: tempUtterance.message
-                    )
-                )
-            }
-        } catch {
-            print(error)
-        }
-        return result
-    }
-    
-    private func makeNewProject() {
-        let newProject = ProjectModel(
-            projectName: "\(Date.now.formatted())",
-            creatAt: Date.now.formatted(),
-            keynotePath: nil,
-            keynoteCreation: "temp"
+        projectManager.stopPractice(
+            mediaManager: mediaManager,
+            keynoteManager: keynoteManager,
+            practiceManager: practiceManager,
+            modelContext: modelContext
         )
-        if let selectedKeynote = keynoteManager.temp {
-            newProject.keynoteCreation = selectedKeynote.creation
-            newProject.keynotePath = URL(fileURLWithPath: selectedKeynote.path)
-            newProject.projectName = selectedKeynote.getFileName()
-        }
-        modelContext.insert(newProject)
-        projectManager.temp = newProject.persistentModelID
-    }
-    
-    private func makeNewPractice(project: ProjectModel, utterances: [UtteranceModel]) -> PracticeModel {
-        /// 새 연습 모델을 생성한다.
-        let result = PracticeModel(
-            practiceName: "init",
-            index: -1,
-            isVisited: false,
-            creatAt: Date().m4aNameToCreateAt(input: mediaManager.fileName),
-            audioPath: mediaManager.getPath(fileName: mediaManager.fileName),
-            utterances: utterances,
-            summary: PracticeSummaryModel()
-        )
-
-        if project.practices.count == 0 {
-            result.index = 0
-            result.practiceName = indexToOrdinalNumber(index: 0)
-        } else {
-            let latestIndex = project.practices.sorted(by: {$0.creatAt > $1.creatAt}).first?.index
-            if let latestIndex = latestIndex {
-                result.index = latestIndex + 1
-                result.practiceName = indexToOrdinalNumber(index: latestIndex + 1)
-            }
-        }
-        return result
-    }
-    
-    private func indexToOrdinalNumber(index: Int) -> String {
-        let ordinalNumber = ["첫", "두", "세", "네", "다섯", "여섯", "일곱", "여덟", "아홉", "열",
-                             "열한", "열두", "열세", "열네", "열다섯", "열여섯", "열일곱", "열여덟"]
-        
-        if ordinalNumber.count < index {
-            return "Index 초과"
-        }
-        return ordinalNumber[index]
     }
             
     private func openSelectedProject() {
