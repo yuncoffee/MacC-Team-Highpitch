3725c5fb5bad89e2c264db987e71958d11b2c4c5
leeyongjun604 <daniel_604@naver.com>
2023-10-29 15:23:08 +0900

    [Refactor] #95 - 주석 수정

--- highpitch/Managers/PracticeManager.swift
+++ highpitch/Managers/PracticeManager.swift
@@ -14,7 +14,7 @@ final class PracticeManager {
 
 extension PracticeManager {
     
-    // 해당 단어가 습관어인지 확인합니다.
+    /// 해당 단어가 습관어인지 확인한다.
     static func isFillerWord(practice: PracticeModel, word: String) -> Bool {
         for (index, fillerWord) in practice.summary.eachFillerWordCount.enumerated() where
         word.last! == "." ? fillerWord.fillerWord + "." == word : fillerWord.fillerWord == word {
@@ -26,14 +26,14 @@ extension PracticeManager {
         return false
     }
     
-    // eachFillerWordCount를 초기화합니다.
+    /// eachFillerWordCount를 초기화한다.
     static func initializer(practice: PracticeModel) {
         for fillerWord in FillerWordList().defaultList {
             practice.summary.eachFillerWordCount.append(FillerWordModel(fillerWord: fillerWord, count: 0))
         }
     }
     
-    // summary에 들어가는 data를 업데이트합니다.
+    /// summary에 들어가는 data를 업데이트한다.
     static func updateSummary(practice: PracticeModel) {
         practice.summary.fastSpeechRate =
         Double(practice.summary.fastSpeechTime * 100) / Double(practice.summary.durationSum)
@@ -43,15 +43,15 @@ extension PracticeManager {
         Double(practice.summary.syllableSum * 60000) / Double(practice.summary.durationSum)
         practice.summary.level = 0
         practice.summary.level += 5 - min(ceil(practice.summary.fillerWordPercentage / 0.03), 4)
-        practice.summary.level += (abs(practice.summary.epmAverage - 356.7) < 20.7 ?  5 : 4 - min(ceil((abs(practice.summary.epmAverage - 356.7) - 20.7) / 25.0), 3))
+        practice.summary.level += (abs(practice.summary.epmAverage - 356.7) < 20.7 ? 5 : 4 - min(ceil((abs(practice.summary.epmAverage - 356.7) - 20.7) / 25.0), 3))
         practice.summary.level /= 2.0
     }
     
     static func getPracticeDetail(practice: PracticeModel) {
-        print("시작!!!!!!")
-        
+        print("분석 시작")
+
         initializer(practice: practice)
-        // sentenceIndex와 wordIndex, sentenceSyllable을 관리합니다.
+        /// sentenceIndex와 wordIndex, sentenceSyllable을 관리한다.
         var sentenceIndex = 0; var wordIndex = 0
         var sentenceSyllable: [Int] = [0]
         var sentenceDuration: [Int] = [0]
@@ -60,8 +60,8 @@ extension PracticeManager {
         
         for (index, utterance) in practice.utterances.sorted().enumerated() {
             let messageLenght = utterance.message.components(separatedBy: " ").count
-            for (index, word) in utterance.message.components(separatedBy: " ").enumerated() {
-                // word와 관련한 값을 업데이트합니다.
+            for word in utterance.message.components(separatedBy: " ") {
+                /// word와 관련한 값을 업데이트한다.
                 practice.summary.wordCount += 1
                 practice.summary.syllableSum += word.count
                 
@@ -72,7 +72,7 @@ extension PracticeManager {
                     sentenceSyllable[sentenceIndex] -= 1
                 }
                 
-                // words, sentences의 sentence를 업데이트합니다.
+                /// words, sentences의 sentence를 업데이트한다.
                 tempWords.append(WordModel(
                     isFillerWord: isFillerWord(practice: practice, word: word),
                     sentenceIndex: sentenceIndex,
@@ -89,20 +89,20 @@ extension PracticeManager {
                     tempSentences[sentenceIndex].sentence += (" " + word)
                 }
                 
-                // 다음 단어로 넘깁니다.
+                /// 다음 단어로 넘어간다.
                 wordIndex += 1
             }
             
-            // duration 및 endAt을 업데이트합니다.
+            /// duration 및 endAt을 업데이트한다.
             tempSentences[sentenceIndex].endAt = utterance.startAt + utterance.duration
             if sentenceIndex == sentenceDuration.count - 1 { sentenceDuration.append(0) }
             sentenceDuration[sentenceIndex] += utterance.duration
             practice.summary.durationSum += utterance.duration
             
-            // 음절의 길이가 15가 넘거나, 마지막 문장이라면 문장을 끝냅니다.
+            /// 음절의 길이가 15가 넘거나, 마지막 문장이라면 문장을 끝낸다.
             if sentenceSyllable[sentenceIndex] > 15 || index == practice.utterances.count - 1 {
                 
-                // EPM을 업데이트합니다.
+                /// EPM을 업데이트한다.
                 tempSentences[sentenceIndex].epmValue =
                 Double(sentenceSyllable[sentenceIndex] * 60000) / Double(sentenceDuration[sentenceIndex])
                 
@@ -116,11 +116,11 @@ extension PracticeManager {
                 tempWords.removeAll()
                 practice.sentences.append(tempSentences[sentenceIndex])
                 
-                // 다음 문장으로 넘깁니다.
+                /// 다음 문장으로 넘긴다.
                 sentenceIndex += 1
             }
         }
         updateSummary(practice: practice)
-        print("끝")
+        print("분석 완료")
     }
 }
